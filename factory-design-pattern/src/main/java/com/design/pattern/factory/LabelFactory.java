a726b2330a83f442b80fa8f5063d67d3429bb5f4
Furqan Ahmad <furqanahmad21@hotmail.com>
2020-07-05 20:40:11 +0500

    created a factory

--- factory-design-pattern/src/main/java/com/design/pattern/factory/LabelFactory.java
+++ factory-design-pattern/src/main/java/com/design/pattern/factory/LabelFactory.java
@@ -1,13 +1,24 @@
-public class LabelFactory {
+package com.design.pattern.factory;
 
-  public LabelPrinter createLabel(Label labelType) {
+import com.design.pattern.product.JuiceBottle;
+import com.design.pattern.work.LabelPrinter;
+import com.design.pattern.product.WaterBottle;
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+
+public abstract class LabelFactory {
+
+  public static LabelPrinter createLabel(Label labelType) {
+    if (labelType == null) {
+      throw new IllegalArgumentException("Please pass a valid argument");
+    }
     switch (labelType) {
       case WATER_BOTTLE:
         return new WaterBottle();
       case JUICE_BOTTLE:
         return new JuiceBottle();
+      default:
+        throw new RuntimeException("Not Implemented yet");
     }
-    throw new IllegalArgumentException("Wrong product chosen");
   }
 
 }
