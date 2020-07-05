c5adb9ac2ec2c3a24c0ab247f83b0b6e3a03254e
Furqan Ahmad <furqanahmad21@hotmail.com>
2020-07-05 20:42:50 +0500

    Checked the implementation of factory design pattern

--- factory-design-pattern/src/main/java/com/design/pattern/Program.java
+++ factory-design-pattern/src/main/java/com/design/pattern/Program.java
@@ -1,10 +1,17 @@
+package com.design.pattern;
+
+import com.design.pattern.factory.Label;
+import com.design.pattern.factory.LabelFactory;
+import com.design.pattern.work.LabelPrinter;
+
 public class Program {
 
   public static void main(String[] args) {
-    LabelFactory labelFactory = new LabelFactory();
-    final LabelPrinter waterBottle = labelFactory.createLabel(Label.WATER_BOTTLE);
+    final LabelPrinter waterBottle = LabelFactory.createLabel(Label.WATER_BOTTLE);
     waterBottle.print();
-    final LabelPrinter juiceBottle = labelFactory.createLabel(Label.JUICE_BOTTLE);
+    final LabelPrinter juiceBottle = LabelFactory.createLabel(Label.JUICE_BOTTLE);
     juiceBottle.print();
+    final LabelPrinter empty = LabelFactory.createLabel(Label.MILK_BOTTLE);
+    empty.print();
   }
 }
